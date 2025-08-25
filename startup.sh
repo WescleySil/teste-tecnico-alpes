852d479294617ffc1fb26872405ac3d19e858048
wescleysil <wescley.silva.cas@gmail.com>
2025-08-25 15:52:20 -0300

    [FIX] Consertando workflow

--- startup.sh
+++ startup.sh
@@ -2,7 +2,6 @@
 
 cp .env.example .env
 cp .env .env.testing
-composer install --no-dev
 php artisan key:generate
 php artisan migrate:fresh --seed
 supervisord -c /etc/supervisor/conf.d/supervisord.conf &
