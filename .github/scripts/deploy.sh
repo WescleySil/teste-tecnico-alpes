2c0b70988c4993249098852e0375e32aeb9b949b
wescleysil <wescley.silva.cas@gmail.com>
2025-08-25 17:36:05 -0300

    [WIP] Teste de deploy automatico

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -3,17 +3,17 @@
 EC2_USER=$AWS_USER
 EC2_HOST=$AWS_HOST
 EC2_PATH=/apps/
-PEM_KEY=~/.ssh/aws_key.pem
 GIT_REPO=https://github.com/WescleySil/teste-tecnico-alpes.git
 BRANCH=main
 
 # Criar a chave PEM a partir do secret
-echo "$AWS_KEY" > $PEM_KEY
-chmod 400 $PEM_KEY
+touch aws_key.pem
+
+echo $AWS_KEY > aws_key.pem
 
 echo "🚀 Iniciando deploy para $EC2_HOST ..."
 
-ssh -i $PEM_KEY $EC2_USER@$EC2_HOST << EOF
+ssh -i aws_key.pem $EC2_USER@$EC2_HOST << EOF
   mkdir -p $EC2_PATH
   cd $EC2_PATH
 
